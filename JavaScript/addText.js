11c6e012d15741ff99cf214cb27c3999cc3b4657
mohsin6862 <mohsinctg2206@gmail.com>
2023-02-11 05:57:23 +0600

    new comment added

--- JavaScript/addText.js
+++ JavaScript/addText.js
@@ -7,3 +7,33 @@ document.getElementById('submit-btn').addEventListener('click',function(){
     updatedText.innerText = inputText;
    textArea.value = "";
 })
+
+
+
+
+document.getElementById('comment-btn').addEventListener('click',function(){
+
+    const textArea = document.getElementById('text-areas');
+    const newcomment = textArea.value;
+
+    const commentContainer = document.getElementById('comment-container');
+    const p = document.createElement('p');
+    p.innerText = newcomment;
+    commentContainer.appendChild(p);
+    textArea.value = '';
+})
+
+
+// document.getElementById('comment-btn').addEventListener('click',function(){
+//     const commentBox = document.getElementById('text-area');
+//     const newComment = commentBox.value;
+
+//     const commentContainer = document.getElementById('comment-container');
+
+//     const p = document.createElement('p');
+//     p.innerText = newComment;
+//     commentContainer.appendChild(p);
+//     commentBox.value = "";
+
+    
+// })
